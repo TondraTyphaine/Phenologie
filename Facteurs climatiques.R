8ab84ee338df611c5c55b39af30db6c6fc979103
Typhaine Tondra <typhaine.tondra@gmail.com>
2024-05-07 17:15:22 +0200

    Ajout de # pour code 1er jeu de données brutes

--- Facteurs climatiques.R
+++ Facteurs climatiques.R
@@ -12,125 +12,125 @@ library(tidyverse)
 library(lubridate)
 library(RColorBrewer)
 
-#### Avec les donnees de M.Badouard 
+#### Avec les donnees de M.Badouard ####
 
 ## Data 
-data<- read_csv2("data/TourAFlux-2004-2023.csv")
+#data<- read_csv2("data/TourAFlux-2004-2023.csv")
 
 #" Reduction des donnees
-data %>% 
-  filter(Year == 2020 : 2023) %>% 
-  select(Year,Month, Day,
-         `Temp(55)`, `Hr(55)`, `Temp(30)`, `Hr(30)`, `Temp(2.5)`, `Hr(2.5)`, 
-         vpd55,
-         Rain, 
-         `ETP penman (mm h-1) - OK`,
-         `ETP (mm h-1) NEW CNR4`,
-         CNR4_temp,
-         TempC_55_CR3000,
-         RH_55_CR3000) ->
-  data
+# data %>% 
+# filter(Year == 2020 : 2023) %>% 
+#  select(Year,Month, Day,
+#         `Temp(55)`, `Hr(55)`, `Temp(30)`, `Hr(30)`, `Temp(2.5)`, `Hr(2.5)`, 
+#         vpd55,
+#        Rain, 
+#        `ETP penman (mm h-1) - OK`,
+#        `ETP (mm h-1) NEW CNR4`,
+#        CNR4_temp,
+#        TempC_55_CR3000,
+#        RH_55_CR3000) ->
+# data
 
 
 ## Moyenne des variables par jour par annee et ajout colonne date
 
 # Pour 2020
-data %>% 
-  filter(Year == 2020) %>% 
-  group_by(Year, Month, Day) %>% 
-  summarise(`Temp(55)` = mean(`Temp(55)`),
-            `Hr(55)`= mean(`Hr(55)`),
-            `Temp(30)`= mean(`Temp(30)`),
-            `Hr(30)`= mean(`Hr(30)`),
-            `Temp(2.5)`= mean(`Temp(2.5)`),
-            `Hr(2.5)`= mean(`Hr(2.5)`),
-            vpd55= mean(vpd55),
-            Rain= mean(Rain),
-            `ETP penman (mm h-1) - OK`= mean(`ETP penman (mm h-1) - OK`),
-            `ETP (mm h-1) NEW CNR4`= mean(`ETP (mm h-1) NEW CNR4`),
-            CNR4_temp= mean(CNR4_temp),
-            TempC_55_CR3000= mean(TempC_55_CR3000),
-            RH_55_CR3000= mean(RH_55_CR3000))%>% 
-  mutate(date = ymd("2020-01-01") + days(Day - 1)) %>%
-  print() ->
-  data2020
+#data %>% 
+#  filter(Year == 2020) %>% 
+#  group_by(Year, Month, Day) %>% 
+# summarise(`Temp(55)` = mean(`Temp(55)`),
+#           `Hr(55)`= mean(`Hr(55)`),
+#           `Temp(30)`= mean(`Temp(30)`),
+#           `Hr(30)`= mean(`Hr(30)`),
+#           `Temp(2.5)`= mean(`Temp(2.5)`),
+#           `Hr(2.5)`= mean(`Hr(2.5)`),
+#           vpd55= mean(vpd55),
+#           Rain= mean(Rain),
+#           `ETP penman (mm h-1) - OK`= mean(`ETP penman (mm h-1) - OK`),
+#           `ETP (mm h-1) NEW CNR4`= mean(`ETP (mm h-1) NEW CNR4`),
+#           CNR4_temp= mean(CNR4_temp),
+#           TempC_55_CR3000= mean(TempC_55_CR3000),
+#           RH_55_CR3000= mean(RH_55_CR3000))%>% 
+# mutate(date = ymd("2020-01-01") + days(Day - 1)) %>%
+# print() ->
+# data2020
 
 
 # Pour 2021
-data %>% 
-  filter(Year == 2021) %>% 
-  group_by(Year, Month, Day) %>% 
-  summarise(`Temp(55)` = mean(`Temp(55)`),
-            `Hr(55)`= mean(`Hr(55)`),
-            `Temp(30)`= mean(`Temp(30)`),
-            `Hr(30)`= mean(`Hr(30)`),
-            `Temp(2.5)`= mean(`Temp(2.5)`),
-            `Hr(2.5)`= mean(`Hr(2.5)`),
-            vpd55= mean(vpd55),
-            Rain= mean(Rain),
-            `ETP penman (mm h-1) - OK`= mean(`ETP penman (mm h-1) - OK`),
-            `ETP (mm h-1) NEW CNR4`= mean(`ETP (mm h-1) NEW CNR4`),
-            CNR4_temp= mean(CNR4_temp),
-            TempC_55_CR3000= mean(TempC_55_CR3000),
-            RH_55_CR3000= mean(RH_55_CR3000))%>%
-  mutate(date = ymd("2021-01-01") + days(Day - 1)) %>%
-  print() ->
-  data2021
+#data %>% 
+# filter(Year == 2021) %>% 
+# group_by(Year, Month, Day) %>% 
+# summarise(`Temp(55)` = mean(`Temp(55)`),
+#           `Hr(55)`= mean(`Hr(55)`),
+#           `Temp(30)`= mean(`Temp(30)`),
+#           `Hr(30)`= mean(`Hr(30)`),
+#           `Temp(2.5)`= mean(`Temp(2.5)`),
+#           `Hr(2.5)`= mean(`Hr(2.5)`),
+#           vpd55= mean(vpd55),
+#           Rain= mean(Rain),
+#           `ETP penman (mm h-1) - OK`= mean(`ETP penman (mm h-1) - OK`),
+#           `ETP (mm h-1) NEW CNR4`= mean(`ETP (mm h-1) NEW CNR4`),
+#           CNR4_temp= mean(CNR4_temp),
+#           TempC_55_CR3000= mean(TempC_55_CR3000),
+#           RH_55_CR3000= mean(RH_55_CR3000))%>%
+# mutate(date = ymd("2021-01-01") + days(Day - 1)) %>%
+#  print() ->
+# data2021
 
 # Pour 2022
-data %>% 
-  filter(Year == 2022) %>% 
-  group_by(Year, Month, Day) %>% 
-  summarise(`Temp(55)` = mean(`Temp(55)`),
-            `Hr(55)`= mean(`Hr(55)`),
-            `Temp(30)`= mean(`Temp(30)`),
-            `Hr(30)`= mean(`Hr(30)`),
-            `Temp(2.5)`= mean(`Temp(2.5)`),
-            `Hr(2.5)`= mean(`Hr(2.5)`),
-            vpd55= mean(vpd55),
-            Rain= mean(Rain),
-            `ETP penman (mm h-1) - OK`= mean(`ETP penman (mm h-1) - OK`),
-            `ETP (mm h-1) NEW CNR4`= mean(`ETP (mm h-1) NEW CNR4`),
-            CNR4_temp= mean(CNR4_temp),
-            TempC_55_CR3000= mean(TempC_55_CR3000),
-            RH_55_CR3000= mean(RH_55_CR3000))%>% 
-  mutate(date = ymd("2022-01-01") + days(Day - 1)) %>%
-  print() ->
-  data2022
+#data %>% 
+# filter(Year == 2022) %>% 
+# group_by(Year, Month, Day) %>% 
+# summarise(`Temp(55)` = mean(`Temp(55)`),
+#           `Hr(55)`= mean(`Hr(55)`),
+#           `Temp(30)`= mean(`Temp(30)`),
+#           `Hr(30)`= mean(`Hr(30)`),
+#           `Temp(2.5)`= mean(`Temp(2.5)`),
+#           `Hr(2.5)`= mean(`Hr(2.5)`),
+#           vpd55= mean(vpd55),
+#           Rain= mean(Rain),
+#           `ETP penman (mm h-1) - OK`= mean(`ETP penman (mm h-1) - OK`),
+#           `ETP (mm h-1) NEW CNR4`= mean(`ETP (mm h-1) NEW CNR4`),
+#           CNR4_temp= mean(CNR4_temp),
+#           TempC_55_CR3000= mean(TempC_55_CR3000),
+#           RH_55_CR3000= mean(RH_55_CR3000))%>% 
+# mutate(date = ymd("2022-01-01") + days(Day - 1)) %>%
+# print() ->
+# data2022
 
 # Pour 2023
-data %>% 
-  filter(Year == 2023) %>% 
-  group_by(Year, Month, Day) %>% 
-  summarise(`Temp(55)` = mean(`Temp(55)`),
-            `Hr(55)`= mean(`Hr(55)`),
-            `Temp(30)`= mean(`Temp(30)`),
-            `Hr(30)`= mean(`Hr(30)`),
-            `Temp(2.5)`= mean(`Temp(2.5)`),
-            `Hr(2.5)`= mean(`Hr(2.5)`),
-            vpd55= mean(vpd55),
-            Rain= mean(Rain),
-            `ETP penman (mm h-1) - OK`= mean(`ETP penman (mm h-1) - OK`),
-            `ETP (mm h-1) NEW CNR4`= mean(`ETP (mm h-1) NEW CNR4`),
-            CNR4_temp= mean(CNR4_temp),
-            TempC_55_CR3000= mean(TempC_55_CR3000),
-            RH_55_CR3000= mean(RH_55_CR3000))%>% 
-  mutate(date = ymd("2023-01-01") + days(Day - 1)) %>%
-  print() ->
-  data2023
+#data %>% 
+# filter(Year == 2023) %>% 
+# group_by(Year, Month, Day) %>% 
+# summarise(`Temp(55)` = mean(`Temp(55)`),
+#           `Hr(55)`= mean(`Hr(55)`),
+#           `Temp(30)`= mean(`Temp(30)`),
+#           `Hr(30)`= mean(`Hr(30)`),
+#           `Temp(2.5)`= mean(`Temp(2.5)`),
+#           `Hr(2.5)`= mean(`Hr(2.5)`),
+#           vpd55= mean(vpd55),
+#           Rain= mean(Rain),
+#           `ETP penman (mm h-1) - OK`= mean(`ETP penman (mm h-1) - OK`),
+#           `ETP (mm h-1) NEW CNR4`= mean(`ETP (mm h-1) NEW CNR4`),
+#           CNR4_temp= mean(CNR4_temp),
+#           TempC_55_CR3000= mean(TempC_55_CR3000),
+#           RH_55_CR3000= mean(RH_55_CR3000))%>% 
+# mutate(date = ymd("2023-01-01") + days(Day - 1)) %>%
+# print() ->
+# data2023
 
 
 ## Empilement des donnees
-bind_rows(data2020) %>% 
-  bind_rows(data2021) %>% 
-  bind_rows(data2022) %>% 
-  bind_rows(data2023) ->
-  data_resume
+#bind_rows(data2020) %>% 
+# bind_rows(data2021) %>% 
+# bind_rows(data2022) %>% 
+# bind_rows(data2023) ->
+# data_resume
 
 
 
   
-### Avec les donnees de M.Bonal ###
+#### Avec les donnees de M.Bonal ####
 
 ## Data 
 dataB<- read_csv2("data/GX-METEO-2020 - 2024E - AK.csv")
@@ -280,11 +280,10 @@ dataB2024 %>%
   Rain2024
 
 
-## Trouver les pics de pluviometrie
+## Trouver les pics de pluie
 
 # Pour 2020
 pic2020 <- sort(findpeaks(Rain2020$Rain, minpeakheight = 2, nups = 1)[,2])
-pic2020_Y <-sort(findpeaks(Rain2020$Rain, minpeakheight = 2, nups = 1)[,1])
 dates2020 <- unique(Rain2020$date)
   
 # Pour 2021
